9ccd1bc4760289e74ca91691aed0aa500bb65e2d
Abdul Azeem <abdulazeem3802@gmail.com>
2024-08-31 01:11:07 +0530

    Second part completed- ui design

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Message from './Message'
+const message = () => {
+  return (
+    <div className='px-4 flex-1 overflow-auto'>
+    <Message />
+    <Message/>
+    <Message/>
+    <Message/>
+    <Message/>
+    <Message/>
+    <Message/>
+    
+    </div>
+  )
+}
+
+export default message
+
+
+
+// STARTER CODE SNIPPET 
+
+// import React from 'react'
+// import Message from './Message'
+// const message = () => {
+//   return (
+//     <div className='px-4 flex-1 overflow-auto'>
+//     <Message />
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     <Message/>
+//     </div>
+//   )
+// }
+
+// export default message
